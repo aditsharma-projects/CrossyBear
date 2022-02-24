d5f0740ef0c524afa8dac2b9b41f5d4fc5504158
Bill Liu <enmingliu@g.ucla.edu>
2022-02-24 03:57:11 -0800

    Added river with logs

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 import {defs, tiny} from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
 class Player extends Shape {
@@ -19,6 +19,52 @@ class Player extends Shape {
         // Arrange the vertices into a square shape in texture space too:
         this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
             14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
+
+class Log {
+    constructor(base_scene, x, z) {
+        this.base_scene = base_scene;
+        this.log_length = 10;
+        this.x = 0;
+        this.dx = 0.02;
+        this.model_transform = Mat4.identity().times(Mat4.rotation(1.57, 0, 1, 0))
+            .times(Mat4.scale(1, 1, this.log_length))
+            .times(Mat4.translation(x, -1.5, z)); 
+    }
+
+    move(t) {
+        this.x -= this.dx;
+        this.model_transform = this.model_transform.times(Mat4.translation(0, 0, -this.dx));
+
+        if (this.x < -10) {
+            this.x = 0;
+            this.model_transform = this.model_transform.times(Mat4.translation(0, 0, 10));
+        }
+    }
+
+    render(context, program_state, t) {
+        this.move(t);
+        this.base_scene.shapes.capped.draw(context, program_state, this.model_transform, this.base_scene.materials.wood);
+    }
+}
+
+class Water {
+    constructor(base_scene) {
+        this.base_scene = base_scene;
+        this.river_width = 5;
+        this.model_tranform = Mat4.identity().times(Mat4.translation(-40, -1.5, -14.7))
+            .times(Mat4.scale(70, 1, this.river_width))
+            .times(Mat4.rotation(1.5, 1, 0, 0));
+    }
+
+    render(context, program_state) {
+        const random = (x) => Math.sin(1000 * x + program_state.animation_time / 1000);
+        this.base_scene.shapes.sheet.arrays.position.forEach((p, i, a) =>
+            a[i] = vec3(p[0], p[1], .15 * random(i / a.length)));
+        this.base_scene.shapes.sheet.flat_shade();
+        this.base_scene.shapes.sheet.draw(context, program_state, this.model_tranform, this.base_scene.materials.water);
+        this.base_scene.shapes.sheet.copy_onto_graphics_card(context.context, ["position", "normal"], false);
     }
 }
 
@@ -130,6 +176,29 @@ class Road {
 }
 
 
+class Water_Shader extends defs.Textured_Phong {
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D texture;
+            void main(){
+                // Sample the texture image in the correct place:
+                vec4 tex_color = texture2D( texture, f_tex_coord );
+                if( tex_color.w < .01 ) discard;
+
+                // Slightly disturb normals based on sampling the same image that was used for texturing:
+                vec3 bumped_N  = N + tex_color.rgb - .5*vec3(1,1,1);
+                // Compute an initial (ambient) color:
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w );
+
+                // Compute the final color with contributions from lights:
+                gl_FragColor.xyz += phong_model_lights( normalize( bumped_N ), vertex_worldspace );
+                gl_FragColor.a = 0.5;
+              } `;
+
+    }
+}
+
 class Base_Scene extends Scene {
     /**
      *  **Base_scene** is a Scene that can be added to any display canvas.
@@ -139,17 +208,28 @@ class Base_Scene extends Scene {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
         this.hover = this.swarm = false;
+
+        // Water grid patch row and column operations
+        const initial_corner_point = vec3(-1, -1, 0);
+        const row_operation = (s, p) => p ? Mat4.translation(0, .2, 0).times(p.to4(1)).to3()
+            : initial_corner_point;
+        const column_operation = (t, p) => Mat4.translation(.2, 0, 0).times(p.to4(1)).to3();
+        
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
             'player': new Player(),
             cube: new defs.Cube(),
             sphere: new defs.Subdivision_Sphere(4),
+            sheet: new defs.Grid_Patch(10, 10, row_operation, column_operation),
+            capped: new defs.Capped_Cylinder(1, 10, [[0, 2], [0, 1]]),
         };
 
         // *** Materials
         this.materials = {
             plastic: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+            water: new Material(new Water_Shader(), {ambient: 1, diffusivity: 1, specularity: 1, texture: new Texture("assets/clear_water.jpg")}),
+            wood: new Material(new defs.Textured_Phong(1), {ambient: .5, texture: new Texture("assets/wood.jpg")}),
         };
         // The white material and basic shader are used for drawing the outline.
         this.white = new Material(new defs.Basic_Shader());
@@ -187,6 +267,10 @@ export class Game extends Base_Scene {
         this.dir = 0;
         this.tStart = -1;
         this.road = new Road(this);
+        this.water = new Water(this);
+        this.log1 = new Log(this, 15, 2);
+        this.log2 = new Log(this, 13, 0);
+        this.log3 = new Log(this, 17, 1);
     }
     set_view() {
         
@@ -271,6 +355,9 @@ export class Game extends Base_Scene {
         let model_transform = Mat4.identity();
         this.render_player(context, program_state, model_transform, t);
         this.road.render(context, program_state, t);
-
+        this.water.render(context, program_state);
+        this.log1.render(context, program_state, t);
+        this.log2.render(context, program_state, t);
+        this.log3.render(context, program_state, t);
     }
 }
